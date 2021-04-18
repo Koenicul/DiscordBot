82c4ea51f5a389eb0bbe9e745245e0ff661f5765
Koenicul <koenvaartjes2005@gmail.com>
2021-04-18 15:11:01 +0200

    Updated abc-formula

--- Commands/abcFormule.py
+++ Commands/abcFormule.py
@@ -13,12 +13,19 @@ async def abc(ctx, a="", b="", c=""):
   except:
     return await ctx.send("Invalid!")
   
-  try:
-    X = (-B + math.sqrt(B**2 - 4*A*C))/(2*A)
-    X2 = (-B - math.sqrt(B**2 - 4*A*C))/(2*A)
-  except:
-    return await ctx.send("Invalid!")
-  await ctx.send("{} or {}".format(floatToString(X), floatToString(X2)))
+  if A == 0:
+      print("That is not a quadratic equation")
+      input()
+  else:
+    try:
+      X = (-B + math.sqrt(B**2 - 4*A*C))/(2*A)
+      X2 = (-B - math.sqrt(B**2 - 4*A*C))/(2*A)
+    except:
+      return await ctx.send("Invalid!")
+    if X != X2:
+      await ctx.send("{} or {}".format(floatToString(X), floatToString(X2)))
+    else:
+      await ctx.send("{}".format(floatToString(X)))
 
 def setup(bot):
   bot.add_command(abc)
